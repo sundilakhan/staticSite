aa5185ba9a8e99b26635750573f3820a4f18cc2e
Sundila Khan <ksundila@gmail.com>
2025-06-24 10:17:25 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function sayHello() {
+  alert("Hello! Thanks for clicking the button.");
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  alert("Form submitted! (but nothing is sent anywhere)");
+}
