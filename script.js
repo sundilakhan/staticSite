2e25abda983ab185f7d3957e679d33b9946161d6
Sundila Khan <ksundila@gmail.com>
2025-06-24 10:58:32 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function handleContact(event) {
+function handleSubmit(event) {
   event.preventDefault();
-  alert("Thanks for reaching out! I’ll get back to you soon.");
+  alert("Thanks for your message! I’ll get back to you soon.");
 }
